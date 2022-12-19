f50ace441aa4c73793810e7dd98dd02c21406811
Vasco <jvcs@ua.pt>
2022-12-19 11:35:32 +0000

    :sparkles: Added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         password: config.get('DB_PASSWORD'),
         database: config.get('DB_NAME'),
         synchronize: true,
+        ssl: true,
       }),
       inject: [ConfigService],
     }),
